59d8890b572b6053114051b2f1d9b7c85b05b444
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-08-25 13:39:33 +0530

    Update alerter.java

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -15,7 +15,7 @@ public class Alerter {
     
     static void alertInCelcius(float farenheit) {
         float celcius = (farenheit - 32) * 5 / 9;
-        int returnCode = networkAlert.sendAlert(celcius)
+        int returnCode = networkAlert.sendAlert(celcius);
         if (returnCode != 200) {
             // non-ok response is not an error! Issues happen in life!
             // let us keep a count of failures to report
