82ba7347f3f7935b6dbe375d17942a387c6d8153
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-08-25 13:34:00 +0530

    Update alerter.java

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -1,12 +1,18 @@
+interface NetworkAlert {
+    int sendAlert(float celcius);
+}
+
+class NetworkAlertStub implements NetworkAlert {
+    @Override
+    public int sendAlert(float celcius) {
+        System.out.println("ALERT: Temperature is " + celcius + " celcius");
+        return (celcius > 200) ? 500 : 200;
+    }
+}
 public class Alerter {
     static int alertFailureCount = 0;
-    static int networkAlertStub(float celcius) {
-        System.out.println("ALERT: Temperature is " + celcius + " celcius");
-        // Return 200 for ok
-        // Return 500 for not-ok
-        // stub always succeeds and returns 200
-        return 200;
-    }
+    static NetworkAlert networkAlert = new NetworkAlertStub();
+    
     static void alertInCelcius(float farenheit) {
         float celcius = (farenheit - 32) * 5 / 9;
         int returnCode = networkAlertStub(celcius);
@@ -21,6 +27,7 @@ public class Alerter {
     public static void main(String[] args) {
         alertInCelcius(400.5f);
         alertInCelcius(303.6f);
+        assert alertFailureCount == 1 : "Expected 1 failure, but got " + alertFailureCount;
         System.out.printf("%d alerts failed.\n", alertFailureCount);
         System.out.println("All is well (maybe!)\n");
     }
